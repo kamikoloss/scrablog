5d022310741885aa523c8a681717215052a342b0
Kamiko Loss <info@gloxi.net>
2023-05-28 03:23:47 +0900

    Tailwind CSS の初期設定を行った

--- scrapbox-nuxt-blog/nuxt.config.ts
+++ scrapbox-nuxt-blog/nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   app: {
     head: {
       htmlAttrs: {
