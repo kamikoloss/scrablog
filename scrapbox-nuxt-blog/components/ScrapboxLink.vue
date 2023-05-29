0e34af513923325fe43c1f88491e6d1ed1e9efee
Kamiko Loss <info@gloxi.net>
2023-05-30 03:23:22 +0900

    ScrapboxLink component を追加した

---
+++ scrapbox-nuxt-blog/components/ScrapboxLink.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({ line: String })
+
+const parseLink = (line) => {
+  const pattern = /[\s\[]https:\S*/
+  const url = line.match(pattern).filter(r => r !== '')[0].replace(/\[|\]|\s/g, '')
+  const title = line.replace(url, '').replace(/\[|\]|\[\s+|\s+\]/g, '')
+  return {title, url}
+}
+const link = parseLink(props.line)
+</script>
+
+<template>
+  <NuxtLink :to="link.url" target="_blank">{{ link.title }}</NuxtLink>
+</template>
