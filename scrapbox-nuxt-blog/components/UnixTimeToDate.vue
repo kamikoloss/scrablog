b6db577181d5e570f6165ae7e4ecfb098abc1f3d
Kamiko Loss <info@gloxi.net>
2023-05-30 00:48:15 +0900

    微修正

--- scrapbox-nuxt-blog/components/UnixTimeToDate.vue
+++ scrapbox-nuxt-blog/components/UnixTimeToDate.vue
@@ -1,8 +1,5 @@
 <script setup>
-const props = defineProps({
-  time: Number,
-})
-
+const props = defineProps({ time: Number })
 const date = new Date(props.time * 1000)
 const dateString = date.toISOString().slice(0, 10)
 </script>
--- scrapbox-nuxt-blog/layouts/default.vue
+++ scrapbox-nuxt-blog/layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
       <header>
       </header>
-      <main class="w-full max-w-3xl m-auto">
+      <main class="w-full max-w-3xl px-4 m-auto">
         <slot />
       </main>
       <footer>        
