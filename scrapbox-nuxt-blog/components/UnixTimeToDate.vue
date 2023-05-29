68d5ded51aabb0c1537cd2aa98239fbcbe804ecf
Kamiko Loss <info@gloxi.net>
2023-05-29 20:58:22 +0900

    UnixTimeToDate component を作成した

---
+++ scrapbox-nuxt-blog/components/UnixTimeToDate.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+  time: Number,
+})
+
+const date = new Date(props.time * 1000)
+const dateString = date.toISOString().slice(0, 10)
+</script>
+
+<template>
+  <span>{{ dateString }}</span>
+</template>
