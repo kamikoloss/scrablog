b688a5edf0d2d2fce9ac40e2dc6812bf2f27e0dd
Kamiko Loss <info@gloxi.net>
2023-05-30 01:37:16 +0900

    YouTube component を追加した

---
+++ scrapbox-nuxt-blog/components/YouTube.vue
@@ -0,0 +1,26 @@
+
+<script setup>
+// TODO: 縮むようにする
+const props = defineProps({ line: Object })
+
+const youtubeUrl = props.line.replace('[', '').replace(']', '')//
+let youtubeId = ''
+if (youtubeUrl.includes('youtube.com')) {
+  youtubeId = youtubeUrl.split('v=').pop()
+} else if (youtubeUrl.includes('youtu.be')) {
+  youtubeId = youtubeUrl.split('/').pop()
+}
+const src = `https://www.youtube.com/embed/${youtubeId}`
+</script>
+
+<template>
+  <iframe
+    width="640"
+    height="360"
+    :src="src"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+  ></iframe>
+</template>
