c57ff0f7b87af57dad9b683ab812cfdb831ff1d6
Kamiko Loss <info@gloxi.net>
2023-05-30 00:39:40 +0900

    GyazoImage component を追加した

---
+++ scrapbox-nuxt-blog/components/GyazoImage.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({ line: String })
+const urlList = props.line.split(']')
+const getSrc = (url) => `${url}/max_size/1000`.replaceAll('[', '')
+const getClass = (url) => ({'max-h-80': !url.startsWith('[[')})
+</script>
+
+<template>
+  <div class="flex">
+    <div v-for="url in urlList">
+      <img v-if="url !== ''" :src="getSrc(url)" :class="getClass(url)" />
+    </div>
+  </div>
+</template>
