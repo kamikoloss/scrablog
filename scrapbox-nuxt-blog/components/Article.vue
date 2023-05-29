47e4f02258717f54a9bd3652d5f24838cc35ecd9
Kamiko Loss <info@gloxi.net>
2023-05-29 21:46:47 +0900

    Article component を追加した (まだまだ途中)

---
+++ scrapbox-nuxt-blog/components/Article.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+const props = defineProps({ page: Object })
+</script>
+
+<template>
+  <article>
+    <div class="border border-gray-300 px-4 py-8">
+      <div class="border-b border-gray-300 pb-4 mb-8">
+        <h2 class="text-xl font-bold">{{ page.title }}</h2>
+        <div class="text-right text-gray-500 ml-auto">
+          <UnixTimeToDate :time="page.created" />
+        </div>
+      </div>
+      <div v-for="(line, index) in page.lines">
+        <!-- 1行目はタイトルなので省略する -->
+        <div v-if="index !== 0">
+          <p class="text-justify leading-relaxed my-4">{{ line }}</p>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
