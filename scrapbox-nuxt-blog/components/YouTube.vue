b41654dad282ac4316107f572972d1fd1bb5930d
Kamiko Loss <info@gloxi.net>
2023-05-30 20:17:51 +0900

    YouTube をレスポンシブに対応させた

--- scrapbox-nuxt-blog/components/YouTube.vue
+++ scrapbox-nuxt-blog/components/YouTube.vue
@@ -1,26 +1,23 @@
 
 <script setup>
-// TODO: 縮むようにする
-const props = defineProps({ line: Object })
+const props = defineProps({ src: String })
 
-const youtubeUrl = props.line.replace(/\[|\]/g, '')
 let youtubeId = ''
-if (youtubeUrl.includes('youtube.com')) {
-  youtubeId = youtubeUrl.split('v=').pop()
-} else if (youtubeUrl.includes('youtu.be')) {
-  youtubeId = youtubeUrl.split('/').pop()
+if (props.src.includes('youtube.com')) {
+  youtubeId = props.src.split('v=').pop()
+} else if (props.src.includes('youtu.be')) {
+  youtubeId = props.src.split('/').pop()
 }
 const src = `https://www.youtube.com/embed/${youtubeId}`
 </script>
 
 <template>
   <iframe
-    width="640"
-    height="360"
     :src="src"
     title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowfullscreen
+    class="w-full aspect-video"
   ></iframe>
 </template>
