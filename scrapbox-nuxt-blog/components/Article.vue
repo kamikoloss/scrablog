f6f2b53b7ed0359cf59a55a803380645e73bc085
Kamiko Loss <info@gloxi.net>
2023-05-30 20:18:46 +0900

    Article を インデント, 1列複数画像, YouTube に対応させた

--- scrapbox-nuxt-blog/components/Article.vue
+++ scrapbox-nuxt-blog/components/Article.vue
@@ -3,7 +3,37 @@
 import { parse } from '@progfay/scrapbox-parser'
 const props = defineProps({ page: Object })
 const lines = parse(props.page.lines.join('\n'))
+console.log(lines)
 
+const lineClass = (line) => {
+  const hasImage = line.nodes?.filter(node => node.type === 'image').length > 0
+  return {
+    'my-2': true,
+    'flex': hasImage,
+  }
+}
+const decorationClass = (decos) => {
+  const isBold = (decos) => {
+    const regex = /\*.*/
+    for (const deco of decos) {
+      if (regex.test(deco)) return true
+    }
+    return false
+  }
+  return {
+    'font-bold': isBold(decos),
+    'text-lg': decos.includes('*-2'),
+    'text-xl': decos.includes('*-3'),
+    'text-2xl': decos.includes('*-4'),
+    'text-3xl': decos.includes('*-5'),
+    'text-4xl': decos.includes('*-6'),
+    'text-5xl': decos.includes('*-7'),
+    'text-6xl': decos.includes('*-8'),
+    'italic': decos.includes('/'),
+    'line-through': decos.includes('-'),
+    'underline': decos.includes('_'),
+  }
+}
 const indentClass = (indent) => {
   return {
     'ml-4': indent === 2,
@@ -29,7 +59,7 @@ const indentClass = (indent) => {
       </div>
       <!-- 本文 -->
       <div class="my-16">
-        <div v-for="line in lines" class="my-1">
+        <div v-for="line in lines" :class="lineClass(line)">
           <!-- 空行 -->
           <div v-if="line.nodes?.length === 0" class="my-8"></div>
           <!-- インデント -->
@@ -43,7 +73,14 @@ const indentClass = (indent) => {
             <!-- リンク -->
             <span v-if="node.type === 'link'">
               <span v-if="node.pathType === 'absolute'">
-                <NuxtLink :to="node.href" class="underline">{{ node.content === '' ? node.href : node.content }}</NuxtLink>
+                <!-- YouTube -->
+                <span v-if="node.href.includes('youtube.com') || node.href.includes('youtu.be')">
+                  <YouTube :src="node.href" />
+                </span>
+                <!-- テキストリンク -->
+                <span v-else>
+                  <NuxtLink :to="node.href" class="underline">{{ node.content === '' ? node.href : node.content }}</NuxtLink>
+                </span>
               </span>
               <span v-if="node.pathType === 'relative'">
                 <!-- TODO -->
@@ -56,12 +93,9 @@ const indentClass = (indent) => {
             </span>
             <!-- 装飾 -->
             <span v-if="node.type === 'decoration'">
-              <span v-for="childNode in node.nodes">
-                <!-- 太字 -->
-                <span v-if="node.decos[0] === '*-1'" class="font-bold">
-                  <span>{{ childNode.text }}</span>
-                  <!-- TODO: プレーンテキストやリンクの区別 -->
-                </span>
+              <span v-for="childNode in node.nodes" :class="decorationClass(node.decos)">
+                <span>{{ childNode.text }}</span>
+                <!-- TODO: プレーンテキストやリンクの区別 -->
               </span>
             </span>
           </span>
