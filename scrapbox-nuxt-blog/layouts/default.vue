5e94ae5cacbedbac32eb942ca665f16e0ad1072d
Kamiko Loss <info@gloxi.net>
2023-05-31 22:14:02 +0900

    default layout にヘッダーとフッターを追加した

--- scrapbox-nuxt-blog/layouts/default.vue
+++ scrapbox-nuxt-blog/layouts/default.vue
@@ -1,11 +1,18 @@
 <template>
-    <div>
-      <header>
-      </header>
-      <main class="w-full max-w-3xl px-4 m-auto">
-        <slot />
-      </main>
-      <footer>        
-      </footer>
-    </div>
+  <div class="text-gray-900 w-full max-w-3xl px-4 mx-auto">
+    <header>
+      <div class="px-4 my-32">
+        <h1 class="font-bold text-2xl">BLOG TITLE</h1>
+        <p>Blog description hogehoge fugafuga.</p>
+      </div>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <div class="text-center my-4">
+        <p class="text-sm text-gray-500">Blog footer hogehoge fufafuga.</p>
+      </div>
+    </footer>
+  </div>
 </template>
