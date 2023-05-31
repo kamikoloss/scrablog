90e6ea35344ab8c46c0adf47483e68cedb6838a3
Kamiko Loss <info@gloxi.net>
2023-05-31 23:41:42 +0900

    Prism plugin を作成した

---
+++ scrapbox-nuxt-blog/plugins/prism.ts
@@ -0,0 +1,12 @@
+import Prism from 'prismjs'
+import 'prismjs/themes/prism-okaidia.css'
+//import 'prismjs/components/prism-javascript'
+
+// https://nuxt.com/docs/guide/directory-structure/plugins
+export default defineNuxtPlugin(() => {
+  return {
+    provide: {
+      prism: () => Prism
+    }
+  }
+})
