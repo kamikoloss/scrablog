edffa35e17ebdb1f30270d72bf5b5c9b9b51c32e
Kamiko Loss <info@gloxi.net>
2023-11-16 15:48:10 +0900

    Paginator コンポーネントを追加した

---
+++ scrapbox-nuxt-blog/components/Paginator.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({ currentNumber: Number })
+
+const newerNumber = Number(props.currentNumber) - 1
+const olderNumber = Number(props.currentNumber) + 1
+</script>
+
+<template>
+  <nav>
+    <div class="flex">
+      <!-- NEWER-->
+      <div class="w-full">
+        <NuxtLink to="/" v-if="newerNumber == 0">&lt;&lt; NEWER</NuxtLink>
+        <NuxtLink :to="`/history/${newerNumber}`" v-if="newerNumber > 0">&lt;&lt; NEWER</NuxtLink>
+      </div>
+      <!-- HISTORY-->
+      <div class="w-full text-center">
+        <NuxtLink :to="`/history`">HISTORY</NuxtLink>
+      </div>
+      <!-- OLDER-->
+      <div class="w-full text-right">
+        <NuxtLink :to="`/history/${olderNumber}`" v-if="olderNumber != lastNumber">OLDER &gt;&gt;</NuxtLink>
+      </div>
+    </div>
+  </nav>
+</template>
