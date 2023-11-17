e92f45807cb758ed5058de93d679eb50563b7c4a
Kamiko Loss <info@gloxi.net>
2023-11-17 23:28:35 +0900

    app config に変数を追加した

--- scrapbox-nuxt-blog/app.config.ts
+++ scrapbox-nuxt-blog/app.config.ts
@@ -1,11 +1,32 @@
-// https://nuxt.com/docs/guide/directory-structure/app-config
+const [ INDEX_TYPE_FULL, INDEX_TYPE_CARD ] = [ 1, 2 ]
+
 export default defineAppConfig({
   // ブログのタイトル
-  blogTitle: 'SCRABLOG SAMPLE',
+  blogTitle: 'Scrablog Sample',
   // ブログのヘッダーの文章
-  headerText: 'SCRABLOG の動作サンプルと使い方です。',
+  headerText: 'Scrablog の動作サンプルと使い方です。',
   // ブログのフッターの文章
   footerText: 'Blog footer text sample',
+
   // 1ページあたり記事をいくつ表示するか
   indexPerPage: 3,
+  // 記事をどのような形式で表示するか (INDEX_TYPE_FULL or INDEX_TYPE_CARD)
+  indexType: INDEX_TYPE_FULL,
+  // 作成日時と更新日時の時間 (12:34 部分) を表示するか (true or false)
+  showTime: true,
+  // 更新日時を表示するか (true or false)
+  showUpdate: false,
+
+  // 文字色
+  textColor: 'gray-900',
+  // 薄い文字色 (引用, フッターの文章)
+  lightTextColor: 'gray-500',
+  // リンクの文字色
+  linkTextColor: 'blue-500',
+  // ブログ全体の背景色
+  backgroundColor: 'gray-200',
+  // 記事部分の背景色
+  articleBackgroundColor: 'white',
+  // 記事内の薄い背景色 (引用, コードブロック, テーブルのヘッダー)
+  lightBackgroundColor: 'gray-100',
 })
