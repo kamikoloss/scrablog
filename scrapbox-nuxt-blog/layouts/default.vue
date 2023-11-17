8269a206d3ee1193d220f9a5332e61d6dbeb832a
Kamiko Loss <info@gloxi.net>
2023-11-17 21:34:41 +0900

    短い記事でも下まで背景色が出るようにした

--- scrapbox-nuxt-blog/layouts/default.vue
+++ scrapbox-nuxt-blog/layouts/default.vue
@@ -3,14 +3,14 @@ const appConfig = useAppConfig()
 </script>
 
 <template>
-  <div class="bg-gray-100 text-gray-900">
+  <div class="min-h-screen bg-gray-200 text-gray-900">
     <header>
       <div class="bg-white py-16">
         <div class="w-full max-w-3xl mx-auto px-8">
           <h1 class="font-bold text-2xl">
             <NuxtLink to="/">{{ appConfig.blogTitle }}</NuxtLink>
           </h1>
-          <p v-if="appConfig.headerText">{{ appConfig.headerText }}</p>
+          <p v-if="appConfig.headerText" class="my-2">{{ appConfig.headerText }}</p>
         </div>
       </div>
     </header>
