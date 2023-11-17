08049b9363aeee12d58d5d0f810068dac42ba658
Kamiko Loss <info@gloxi.net>
2023-11-18 00:50:32 +0900

    config ファイル取得の API エンドポイントの間違いを修正した

--- scrapbox-downloader/index.js
+++ scrapbox-downloader/index.js
@@ -3,13 +3,16 @@ import fs from 'fs'
 
 // https://scrapbox.io/scrapboxlab/api%2Fpages%2F:projectname
 // https://scrapbox.io/scrapboxlab/api%2Fpages%2F:projectname%2F:pagetitle
+// https://scrapbox.io/scrapboxlab/api%2Fcode%2F:projectname%2F:pagetitle%2F:filename
 let api = null
+let projectName = ''
+let connectSid = ''
 
 // Scrapbox の API インスタンスを作成する
 // 事前に環境変数を設定する必要がある
-const createApiInstance = (projectName, connectSid) => {
+const createApiInstance = (connectSid) => {
   return axios.create({
-    baseURL: `https://scrapbox.io/api/pages/${projectName}`,
+    baseURL: `https://scrapbox.io/api`,
     timeout: 10000, // 10s
     headers: { 'Cookie': `connect.sid=${connectSid};` },
   })
@@ -18,7 +21,7 @@ const createApiInstance = (projectName, connectSid) => {
 // Scrapbox のページの一覧情報を取得する
 const getScrapboxPageList = async () => {
   return await api
-    .get('', { params: { limit: 1000 } })
+    .get(`/pages/${projectName}`, { params: { limit: 1000 } })
     .then(response => response.data?.pages)
 }
 
@@ -27,7 +30,7 @@ const getScrapboxPages = async (pageList) => {
   return await Promise
     .all(pageList
       .filter(isDownloadTargetPage)
-      .map(page => api.get(`/${encodeURIComponent(page.title)}`))
+      .map(page => api.get(`/pages/${projectName}/${encodeURIComponent(page.title)}`))
     )
     .then(responses => {
       return responses.map(response => {
@@ -58,26 +61,27 @@ const makePageJsonFiles = (pages) => {
 
 // config ページ内のコードを取得してファイルを作成する
 const makeAppConfigFile = async () => {
-  const fileName = `./content/app.config.ts`
-  const codeText = await api.get('/config/app.config.ts').then(response => response.data)
+  const fileName = './content/app.config.ts'
+  const codeText = await api.get(`/code/${projectName}/config/app.config.ts`).then(response => response.data)
   fs.writeFileSync(fileName, codeText)
 }
 
 // 引数を取得する
-const projectName = process.argv[2]
-const connectSid = process.argv[3]
+projectName = process.argv[2]
+connectSid = process.argv[3]
 
 // メイン処理
 // node index.js <ScrapboxProjectName> <ScrapboxConnectSid>
 try {
-  api = createApiInstance(projectName, connectSid)
+  api = createApiInstance(connectSid)
   const pageList = await getScrapboxPageList()
   console.log(`Succeed to get page list (${pageList.length})`)
   const pages = await getScrapboxPages(pageList)
-  console.log(`Succeed to get pages`)
+  console.log('Succeed to get pages')
   makePageJsonFiles(pages)
   await makeAppConfigFile()
-  console.log(`Complete!`)
+  console.log('Succeed to make files')
+  console.log('Complete!')
 } catch (error) {
   console.error(error)
 }
