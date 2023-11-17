2f2af7c89b7a3a14a2069ad330b041a0b1a5d1a4
Kamiko Loss <info@gloxi.net>
2023-11-17 23:38:42 +0900

    微修正

--- scrapbox-nuxt-blog/components/YouTube.vue
+++ scrapbox-nuxt-blog/components/YouTube.vue
@@ -3,20 +3,22 @@
 const props = defineProps({ src: String })
 
 // YouTube の埋め込み用 URL を作成する
-let youtubeId = ''
-if (props.src.includes('youtube.com')) {
-  // youtube.com の場合: 'v=' 以降の動画 ID を取得する 
-  youtubeId = props.src.split('v=').pop()
-} else if (props.src.includes('youtu.be')) {
-  // youtu.be の場合: '/' 以降の動画 ID を取得する 
-  youtubeId = props.src.split('/').pop()
+const getYouTubeSrc = (src) => {
+  let id = ''
+  if (src.includes('youtube.com')) {
+    // youtube.com の場合: 'v=' 以降の動画 ID を取得する 
+    id = src.split('v=').pop()
+  } else if (src.includes('youtu.be')) {
+    // youtu.be の場合: '/' 以降の動画 ID を取得する 
+    id = src.split('/').pop()
+  }
+  return `https://www.youtube.com/embed/${id}`
 }
-const src = `https://www.youtube.com/embed/${youtubeId}`
 </script>
 
 <template>
   <iframe
-    :src="src"
+    :src="getYouTubeSrc(src)"
     title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
