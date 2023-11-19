4e766aade8c206f62429f820c29ba3acc9a73fff
Kamiko Loss <info@gloxi.net>
2023-11-19 18:45:37 +0900

    Google Fonts の設定を修正した

--- scrapbox-nuxt-blog/nuxt.config.ts
+++ scrapbox-nuxt-blog/nuxt.config.ts
@@ -14,9 +14,13 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com',
         },
         {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: '',
+        },
+        {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap',
-          crossorigin: '',
         },
         {
           rel: 'stylesheet',
