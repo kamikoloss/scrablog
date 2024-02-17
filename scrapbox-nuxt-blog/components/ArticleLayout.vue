0b6c9edc3965c4d37bb54a0e1bc90052cfc1812b
Kamiko Loss <info@gloxi.net>
2024-02-18 06:34:24 +0900

    ArticleLayout で header が省略されたときに border を表示しないようにした

--- scrapbox-nuxt-blog/components/ArticleLayout.vue
+++ scrapbox-nuxt-blog/components/ArticleLayout.vue
@@ -1,6 +1,11 @@
+<script setup>
+const slots = useSlots()
+const hasSlot = (name) => slots[name] !== undefined
+</script>
+
 <template>
   <article class="bg-bg-content px-8 py-16">
-    <div class="border-b border-gray-300 mb-16">
+    <div v-if="hasSlot('header')" class="border-b border-gray-300 mb-16">
       <slot name="header" />
     </div>
     <div>
