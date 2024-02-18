1cd3d698a96f0db7acbee01924d58b2ded4a371f
Kamiko Loss <info@gloxi.net>
2024-02-19 07:11:31 +0900

    ArticleLayout に footer と bottom の slot を追加した

--- scrapbox-nuxt-blog/components/ArticleLayout.vue
+++ scrapbox-nuxt-blog/components/ArticleLayout.vue
@@ -4,12 +4,20 @@ const hasSlot = (name) => slots[name] !== undefined
 </script>
 
 <template>
-  <article class="bg-bg-content px-8 py-16">
-    <div v-if="hasSlot('header')" class="border-b border-gray-300 mb-16">
-      <slot name="header" />
+  <div>
+    <article class="bg-bg-content px-8 py-16">
+      <div v-if="hasSlot('header')" class="border-b border-gray-300 mb-16">
+        <slot name="header" />
+      </div>
+      <div>
+        <slot name="main" />
+      </div>
+      <div v-if="hasSlot('footer')" class="border-t border-gray-300 mt-16">
+        <slot name="footer" />
+      </div>
+    </article>
+    <div v-if="hasSlot('bottom')" class="mt-16 px-8">
+      <slot name="bottom" />
     </div>
-    <div>
-      <slot name="main" />
-    </div>
-  </article>
+  </div>
 </template>
