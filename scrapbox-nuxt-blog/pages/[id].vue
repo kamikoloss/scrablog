b618b0db205802ccc145f1630ee51e31487dfe4e
Kamiko Loss <info@gloxi.net>
2024-02-22 07:51:20 +0900

    [title] -> [id] にした, id を route で取るようにした

---
+++ scrapbox-nuxt-blog/pages/[id].vue
@@ -0,0 +1,33 @@
+<script setup>
+const appConfig = useAppConfig()
+
+const route = useRoute()
+const { id } = route.params
+
+const { data: article } = await useAsyncData(`index-${id}`, () => {
+  return whereNotInTitle(queryContent())
+    .where({ id })
+    .findOne()
+})
+const { data: surround } = await useAsyncData(`index-${id}-surround`, () => {
+  return whereNotInTitle(queryContent())
+    .sort({ created: -1 })
+    .findSurround(`/${id}`)
+})
+
+useHead({
+  title: `${appConfig.blogTitle} - ${article.value.title}`,
+})
+</script>
+
+<template>
+  <div>
+    <Article
+      v-if="article"
+      :article="article"
+      :surround="surround"
+      class="my-32"
+    />
+    <ArticleNotFound v-else class="my-32" />
+  </div>
+</template>
