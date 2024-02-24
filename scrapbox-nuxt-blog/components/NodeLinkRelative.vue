6903f8520a58fe517fcdd67ea250b9ec698a5747
Kamiko Loss <info@gloxi.net>
2024-02-24 11:31:31 +0900

    links へのリンクをエスケープした

--- scrapbox-nuxt-blog/components/NodeLinkRelative.vue
+++ scrapbox-nuxt-blog/components/NodeLinkRelative.vue
@@ -7,8 +7,12 @@ const { data: allArticles } = await useAsyncData('node-link-relative', () => {
 
 const articleId = allArticles.value.find(article => article.title === props.node?.href)?.id
 const isArticleLink = articleId !== undefined
-const linkTo = isArticleLink ? `/${articleId}` : `/links/${props.node?.href}`
-const linkText = props.node?.type === 'hashTag' ? props.node?.raw : props.node?.href
+const linkTo = isArticleLink
+  ?`/${articleId}`
+  : `/links/${escapeArticleTitle(props.node?.href)}`
+const linkText = props.node?.type === 'hashTag'
+  ? props.node?.raw
+  : props.node?.href
 </script>
 
 <template>
--- scrapbox-nuxt-blog/pages/links/[[title]].vue
+++ scrapbox-nuxt-blog/pages/links/[[title]].vue
@@ -1,15 +1,18 @@
 <script setup>
 const route = useRoute()
-const { title } = route.params
+const { title: routeTitle } = route.params
+const unescapedTitle = unescapeArticleTitle(routeTitle)
 
 const { data: articles } = await useAsyncData('links', () => {
   return whereNotInTitle(queryContent()).find()
 })
 
+// title のリンクを持つ記事一覧を取得する
 const relatedArticles = []
 articles.value.forEach(article => {
   article.links.forEach(linkTitle => {
-    if (linkTitle === title && !relatedArticles.some(a => a.title === title)) {
+    const isFirst = !relatedArticles.some(a => a.title === unescapedTitle)
+    if (linkTitle === unescapedTitle && isFirst) {
       relatedArticles.push(article)
     }
   })
@@ -19,7 +22,7 @@ articles.value.forEach(article => {
 <template>
   <div class="my-32">
     <div class="text-text-light px-8">
-      <h2 class="text-xl font-bold my-2">"{{ title }}" に関連する記事</h2>
+      <h2 class="text-xl font-bold my-2">"{{ unescapedTitle }}" に関連する記事</h2>
       <p class="my-2">{{ relatedArticles.length }}件見つかりました。</p>
     </div>
     <Card v-for="article in relatedArticles" :article="article" class="my-8" />
