baff66ea5c50a939b73a22b0cfa42786aaeb179c
Kamiko Loss <info@gloxi.net>
2024-02-28 22:06:41 +0900

    const と config に header nav と sidebar の項目を追加した

--- scrapbox-nuxt-blog/scrablog.const.js
+++ scrapbox-nuxt-blog/scrablog.const.js
@@ -1,15 +1,28 @@
-const indexTypes = {
+export const indexTypes = {
   BLOG_FULL: 1,
   BLOG_CARD: 2,
 }
 
-const sideBarContents = {
-  PROFILE: 1,
+export const headerNavTypes = {
+  ARTICLE: 1,
   SEARCH: 2,
-  RECENT_ARTICLES: 3,
-  ARTICLES_CATEGORY: 4,
-  ARTICLES_MONTH: 5,
-  ARTICLES_YEAR: 6,
+  ALL_ARTICLES: 3,
+  ALL_IMAGES: 4,
+  ALL_TAGS: 5,
 }
 
-export { indexTypes, sideBarContents }
+export const sidebarPositions = {
+  LEFT: 1,
+  RIGHT: 2,
+  BOTTOM: 3,
+}
+
+export const sidebarTypes = {
+  ARTICLE: 1,
+  SEARCH: 2,
+  RECENT_ARTICLES: 3,
+  ALL_ARTICLES_YEAR: 4,
+  ALL_ARTICLES_MONTH: 5,
+  ALL_ARTICLES_YEAR_MONTH: 6,
+  POPULAR_TAGS: 7,
+}
