4abed5f019401cbf26a39a5604ff95dc6c697a35
Kamiko Loss <info@gloxi.net>
2024-03-01 23:59:50 +0900

    /history -> /articles, ラベルを config から取るようにした

--- scrapbox-nuxt-blog/pages/articles.vue
+++ scrapbox-nuxt-blog/pages/articles.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { headerNavTypes, defaultAllArticlesLabel } from '~/scrablog.const';
+
 const appConfig = useAppConfig()
 
+const allArticlesConfig = appConfig.headerNavContents.find(c => c.type === headerNavTypes.ALL_ARTICLES)
+const allArticlesLabel = allArticlesConfig?.label ?? defaultAllArticlesLabel
+
 useHead({
-  title: `${appConfig.blogTitle} - HISTORY`,
+  title: `${appConfig.blogTitle} - ${allArticlesLabel}`,
 })
 
-const { data: articles } = await useAsyncData('history', () => {
+const { data: articles } = await useAsyncData('articles', () => {
   return whereNotInTitle(queryContent()).sort({ created: -1 }).find()
 })
 </script>
@@ -13,7 +18,7 @@ const { data: articles } = await useAsyncData('history', () => {
 <template>
   <ArticleLayout class="my-32">
     <template #header>
-      <h2 class="text-xl font-bold my-2">HISTORY</h2>
+      <h2 class="text-xl font-bold my-2">{{ allArticlesLabel }}</h2>
     </template>
     <template #main>
       <ul>
