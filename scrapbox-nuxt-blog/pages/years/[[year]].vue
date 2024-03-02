844e63fb8a270d84ac27c5d2a767f2b47d3518df
Kamiko Loss <info@gloxi.net>
2024-03-02 22:52:33 +0900

    年別と月別の記事カード一覧ページを作成した

---
+++ scrapbox-nuxt-blog/pages/years/[[year]].vue
@@ -0,0 +1,20 @@
+<script setup>
+const route = useRoute()
+const { year: routeYear } = route.params
+
+const { data: articles } = await useAsyncData('years', () => {
+  return whereNotInTitle(queryContent()).sort({ created: -1 }).find()
+})
+const articlesYearMonth = getArticlesGroupByYearMonth(articles.value)
+const articlesYear = articlesYearMonth.year[routeYear]
+</script>
+
+<template>
+  <div class="my-32">
+    <div class="text-text-light px-8">
+      <h2 class="text-xl font-bold my-2">"{{ routeYear }}" に書かれた記事</h2>
+      <p class="my-2">{{ articlesYear.length }} 件見つかりました。</p>
+    </div>
+    <Card v-for="article in articlesYear" :article="article" class="my-8" />
+  </div>
+</template>
